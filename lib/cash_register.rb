bea94ada49b35563f3c8b145d23571c316f48c37
Sara Kantor <sara.kantor.nv@gmail.com>
2018-12-06 21:21:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
-
+class CashRegister 
+  
+  attr_accessor :title, :price, :employee_discount
+  
+  def initialize(employee_discount=0)
+    @total = 0 
+  end 
+  
+  def total 
+    total 
+  end 
+  
+  def add_item(title, price, quantity=1)
+    @total = @total + (price * quantity)
+  end 
+  
+  def apply_discount
+    @total * 0.80
+    puts "Congrats"
+  end 
+  
+  def items 
+    @items = []
+  end
+  
+  def void_last_transaction 
+  end 
+  
+end 
